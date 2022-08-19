d7824c53a2721c8b50c839acc4c9bb2b3aef42ab
Sergei Johansen <sergeitheprogrammer@gmail.com>
2022-08-19 17:30:13 +0200

    Checking deployment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 // eslint-disable-next-line no-unused-vars
+import React from "react";
 import firebase from "./FirebaseConfig";
 
 function App() {
   return (
-    <h1>This is my app for deploying on Firebase</h1>
+    <React.Fragment>
+
+      <h1>This is my app for deploying on Firebase</h1>
+      <p>Checking auto deploy</p>
+    </React.Fragment>
   );
 }
 
